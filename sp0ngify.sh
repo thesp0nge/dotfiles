fc2abfbdacde25cf564f42ac714276cca1cc8382
Paolo Perego <paolo@codiceinsicuro.it>
2021-12-16 17:58:14 +0100

    First import

---
+++ sp0ngify.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+WGET=`which wget`
+CONF_DST=$HOME"/.config"
+if [ ! -x $WGET ]; then
+    echo "[!] missing wget. I don't know how to download files"
+    exit 1
+fi
+
+echo "[+] Installing TMUX in .config/tmux"
+TMUX_DATA="https://github.com/thesp0nge/dotfiles/blob/e4516e6238cd5969132913b7c985d54703b4ad5d/data/tmux.tar.gz"
+TMUX_DST=$CONF_DST"/tmux"
+TMUX_LN=$HOME"/.tmux.conf"
+
+if [ -d $TMUX_DST ]; then
+    echo "[!] $TMUX_DST exists. Giving up"
+    exit 1
+fi
+
+if [ -x  $TMUX_LN ]; then
+    echo "[!] $TMUX_LN exists. Giving up"
+    exit 1
+fi
+
+mkdir -pv $TMUX_DST
+$WGET $TMUX_DATA
+tar xfv tmux.tar.gz -C $CONF_DST
+
+if [ -f $TMUX_DST"/tmux.conf" ]; then
+    echo "[+] Ok"
+else
+    echo "[!] Failed."
+    exit 1
+fi
+
+ln -s $TMUX_DST"/tmux.conf" $TMUX_LN
+
+echo $TMUX_DST
